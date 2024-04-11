4e3e123244d9dae7b6831910e8462b76fcab97d6
wessel <wesselvandalen@gmail.com>
2024-04-12 00:05:29 +0200

    Added spanish language, fixed .form on laptop, added projects by comments

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ function changeLanguage(lang) {
         changeLanguageDutch();
     } else if (lang === 'no') {
         changeLanguageNorwegian();
+    } else if (lang === 'es') {
+        changeLanguageEspanol();
     } else {
         changeLanguageEnglish();
     }
@@ -66,6 +68,7 @@ function buildSomething() {
     const nlKnop = document.querySelector('#nlknop');
     const enknop = document.querySelector('#enknop');
     const noknop = document.querySelector('#noknop');
+    const esknop = document.querySelector('#esknop');
 
     // https://phuoc.ng/collection/html-dom/change-the-favicon-dynamically-based-on-user-color-scheme-preference/
     const setFavicon = () => {
@@ -89,6 +92,11 @@ function buildSomething() {
     noknop.addEventListener('click', (ev) => {
         changeLanguageNorwegian();
         changeLanguage('no');
+    });
+
+    esknop.addEventListener('click', (ev) => {
+        changeLanguageEspanol();
+        changeLanguage('es');
     });
 
     popupBtn.addEventListener('click', openPopup);
@@ -138,7 +146,6 @@ function changeLanguageDutch() {
     document.getElementById('projects-h2-text').textContent = "PROJECTEN";
     document.getElementById('project-p').textContent = "Ieder project is een uniek stuk development🎨";
 
-    document.getElementById('kpn-title').textContent = "KPN Reisbewegingen";
     document.getElementById('kpn-description').innerHTML = `
                     KPN needed a website for its employees to register their kilometers, due to the Dutch government asking coorperations to do so because of <a target="_blank" class="skill-a" href="https://www.rvo.nl/onderwerpen/rapportage-wpm"> this law</a>. 
                     The application then calculates the co2 emissions, and shows this on a dashboard for the managers to see. It also creates an Excel sheet report containing all the co2 emissions of all employees for KPN to deliver.
@@ -182,7 +189,6 @@ function changeLanguageEnglish() {
     document.getElementById('projects-h2-text').textContent = "PROJECTS";
     document.getElementById('project-p').textContent = "Each project is a unique piece of development🎨";
 
-    document.getElementById('kpn-title').textContent = "KPN Travel movements";
     document.getElementById('kpn-description').innerHTML = `
                     KPN needed a website for its employees to register their kilometers, due to the Dutch government asking coorperations to do so because of <a target="_blank" class="skill-a" href="https://www.rvo.nl/onderwerpen/rapportage-wpm"> this law</a>. 
                     The application then calculates the co2 emissions, and shows this on a dashboard for the managers to see. It also creates an Excel sheet report containing all the co2 emissions of all employees for KPN to deliver.
@@ -226,7 +232,6 @@ function changeLanguageNorwegian() {
     document.getElementById('projects-h2-text').textContent = "PROSJEKT";
     document.getElementById('project-p').textContent = "Hvert prosjekt er et unikt stykke utvikling🎨";
 
-    document.getElementById('kpn-title').textContent = "KPN Reisebevegelser";
     document.getElementById('kpn-description').innerHTML = `
                     KPN trengte et nettsted for sine ansatte for å registrere kilometerne sine, på grunn av at den nederlandske regjeringen ba samarbeid om å gjøre det på grunn av <a target="_blank" class="skill-a" href="https://www.rvo.nl/onderwerpen/rapportage-wpm"> denne loven</a>. 
                     Applikasjonen beregner deretter co2-utslippene, og viser dette på et dashbord som lederne kan se. Den lager også en Excel-arkrapport som inneholder alle co2-utslippene til alle ansatte som KPN kan levere.                   
@@ -246,4 +251,44 @@ function changeLanguageNorwegian() {
     document.querySelector('#mail-a').textContent = 'Send meg en e-post';
     document.querySelector('#cv-a').textContent = 'Se på CV-en min';
     document.querySelector('#popupBtn').textContent = 'Se på studien min';
+}
+function changeLanguageEspanol() {
+    document.documentElement.lang = 'es';
+
+    // top div
+    document.getElementById('home-a').textContent = 'Página principal';
+    document.getElementById('projects-a').textContent = 'Proyectos';
+    document.getElementById('contact-a').textContent = 'Contacto';
+    document.getElementById('language-a').textContent = 'Idiomas';
+
+    // home box
+    document.getElementById('personal-description').textContent = `
+    ¡Hola! Mi nombre es Wessel y soy un chico holandés de ${calculateAge()} años. 
+    Me gusta pasear por el bosque con mi perro, ir al gimnasio y diseñar y crear aplicaciones web.
+    `;
+
+    // tech
+    document.getElementById('tech-stack').textContent = "Una colección de tecnologías que he utilizado a lo largo de mis proyectos💡";
+
+    // projects
+    document.getElementById('projects-h2-text').textContent = "PROYECTOS";
+    document.getElementById('project-p').textContent = "Cada proyecto es una pieza única de desarrollo🎨";
+
+    document.getElementById('kpn-description').innerHTML = `
+                    KPN necesitaba un sitio web para que sus empleados registraran sus kilómetros, debido a que el gobierno holandés pidió a las cooperativas que lo hicieran debido a <a target="_blank" class="skill-a" href="https://www.rvo.nl/onderwerpen/rapportage-wpm">esta ley</a>. 
+                    Luego, la aplicación calcula las emisiones de CO2 y las muestra en un panel para que las vean los gerentes. También crea un informe en hoja de Excel que contiene todas las emisiones de CO2 de todos los empleados para que KPN lo entregue.
+                    <br>
+                    Dirección de correo electrónico: manager1@gmail.com<br>
+                    Contraseña: managerPass`;
+
+    document.getElementById('last-text').textContent = "Y muchos más proyectos por venir...";
+
+    // degree
+    document.getElementById('popup-bachelor').textContent = "Bachelor • 4 años";
+
+    // footer
+    document.querySelector('#footer-p').textContent = '¿Interesado?';
+    document.querySelector('#mail-a').textContent = 'Envíame un correo electrónico';
+    document.querySelector('#cv-a').textContent = 'Mira mi CV';
+    document.querySelector('#popupBtn').textContent = 'Ver mi estudio';
 }
