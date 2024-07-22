e666325945ad125adffabeafed5ce409c909e37d
wessel <wesselvandalen@gmail.com>
2024-07-22 22:49:22 +0200

    Small changes

--- index.js
+++ index.js
@@ -38,10 +38,15 @@ function calculateAge() {
 
 function renderText() {
     document.querySelector('#copyright').textContent = `© ${new Date().getFullYear()} Wessel van Dalen, Some rights reserved.`; // https://www.termsfeed.com/blog/sample-copyright-notices/
-    document.getElementById('about-description').innerHTML = `Heisann! <a class="special-text" target="_blank" href="https://en.wikipedia.org/wiki/Norwegian_language">*Heisann is Noors voor hallo</a><br><br>
-                            Mijn naam is Wessel, ik ben ${calculateAge()} jaar, en geboren in Houten, Nederland.
-                            Ik heb <a class="special-text" href="https://www.hu.nl/voltijd-opleidingen/hbo-ict" target="_blank">HBO ICT</a> gestudeerd op de <a class="special-text" href="https://www.hu.nl/" target="_blank">Hogeschool Utrecht</a>
-                            om een full-stack developer te worden gespecialiseerd in back-end.
-                            <br><br>
-                            Ik ga graag naar het bos met mijn hond Rowdy (Golden Retriever), vind het leuk om te schaken, game graag met vrienden en leer graag Noors (taal, cultuur, etc.).`;
+    document.getElementById('about-description').innerHTML =
+        `Heisann! <a class="special-text" target="_blank" href="https://en.wikipedia.org/wiki/Norwegian_language">*Heisann is Noors voor hallo</a><br><br>
+        Mijn naam is Wessel, ik ben ${calculateAge()} jaar en ben geboren in Houten, Nederland.
+        Ik heb <a class="special-text" href="https://www.hu.nl/voltijd-opleidingen/hbo-ict" target="_blank">HBO ICT</a> gestudeerd op de <a class="special-text" href="https://www.hu.nl/" target="_blank">Hogeschool Utrecht</a>
+        om een Full Stack Software Developer te worden gespecialiseerd in back-end.
+        <br><br>
+        Ik ga graag naar het bos met mijn hond Rowdy (Golden Retriever), vind het leuk om te schaken, train graag een paar keer per week in de sportschool, game regelmatig met vrienden en houdt erg veel van Noors (taal, cultuur, etc.).
+        <br><br>
+        Ik ben erg enthousiast als het gaat om IT, ben altijd op de hoogte van de laatste nieuwtjes en ben erg leergierig. Ik vind het daarom ook leuk om op allerlei verschillende vlakken werkend te zijn, en vind afwisseling
+        daarom ook erg fijn in mijn werk. 
+       `;
 }
