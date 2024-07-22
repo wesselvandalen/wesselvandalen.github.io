dc9e3ab0d46f97895a4841cc05d5e737d5aa7517
wessel <wesselvandalen@gmail.com>
2024-07-22 23:20:27 +0200

    Last small fixes, tomorrow phone update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function renderText() {
         <br><br>
         Ik ga graag naar het bos met mijn hond Rowdy (Golden Retriever), vind het leuk om te schaken, train graag een paar keer per week in de sportschool, game regelmatig met vrienden en houdt erg veel van Noors (taal, cultuur, etc.).
         <br><br>
-        Ik ben erg enthousiast als het gaat om IT, ben altijd op de hoogte van de laatste nieuwtjes en ben erg leergierig. Ik vind het daarom ook leuk om op allerlei verschillende vlakken werkend te zijn, en vind afwisseling
+        Ik ben erg enthousiast als het gaat om IT, ik ben altijd op de hoogte van de laatste nieuwtjes en ik ben erg leergierig. Daarom vind ik het ook leuk om op allerlei verschillende vlakken werkend te zijn, en vind afwisseling
         daarom ook erg fijn in mijn werk. 
        `;
 }
