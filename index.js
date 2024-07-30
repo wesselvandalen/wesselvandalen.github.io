2c397aff545ad2bdf27ee42dca4d0239d0675f10
wessel <wesselvandalen@gmail.com>
2024-07-31 01:03:21 +0200

    Fixed cards + projects + tech stack

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 addEventListener("DOMContentLoaded", (event) => {
     setTimeout(() => {
-        renderText();
+        document.getElementById('footer-rights').innerHTML = `&copy; ${new Date().getFullYear()} Wessel. All rights reserved.`;
     }, 100);
 });
-
-function calculateAge() {
-    let dob = new Date("01/20/2004");
-    let month_diff = Date.now() - dob.getTime();
-    let age_dt = new Date(month_diff);
-    let year = age_dt.getUTCFullYear();
-    return Math.abs(year - 1970);
-}
-
-function renderText() {
-    document.getElementById('footer-rights').innerHTML = `&copy; ${new Date().getFullYear()} Wessel. All rights reserved.`;
-    document.getElementById('about-description').innerHTML =
-        `Heisann! *Heisann is Noors voor hallo<br><br>
-        Mijn naam is Wessel, ik ben ${calculateAge()} jaar oud en ik ben geboren in Houten, Nederland.
-        Ik heb HBO ICT gestudeerd op de Hogeschool Utrecht om een Full Stack Software Developer te worden gespecialiseerd in back-end.
-        <br><br>
-        Ik ga graag naar het bos met mijn hond Rowdy (Golden Retriever), vind het leuk om te schaken, train graag een paar keer per week in de sportschool, game regelmatig met vrienden en houdt erg veel van Noors (taal, cultuur, etc.). 
-       `;
-}
