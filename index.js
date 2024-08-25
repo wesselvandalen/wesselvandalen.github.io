b97737da070f361a02a70d950a9caf3146be694c
wessel <wesselvandalen@gmail.com>
2024-08-25 13:06:12 +0200

    Imidlertidig update

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-addEventListener("DOMContentLoaded", (event) => {
+addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        document.getElementById('description').textContent = `A social ${calculateAge()} year old Full Stack Software Developer with a Norwegian touch, eager to learn.`;
+        document.getElementById('description').textContent = `A social ${calculateAge()} year old Full Stack Software Developer with a Norwegian charm, enthousiastic to discover and create.`;
         document.getElementById('about').innerHTML = `
-            Hei hei!<br> My name is Wessel and I'm a ${calculateAge()} year old guy from the Netherlands. I'm social, hard-working and serious, but always down for a laugh. 
+            Hei hei!<br> My name is Wessel and I'm a ${calculateAge()} year old guy with a Norwegian charm born in the Netherlands. I'm socially engaged, always down to create a good team-spirit and take great joy and pride in the code I write! 
             <br><br>
-            I am a Full Stack Software Engineer specialized in front end. I have a great passion for software development, having studied the bachelor <a class="about-a" href="https://www.hu.nl/voltijd-opleidingen/hbo-ict" target="_blank">HBO ICT - Software Development</a> at the Hogeschool Utrecht (2022 - 2026).
+            I am a Full Stack Software Engineer with a bachelor in <a class="about-a" href="https://www.hu.nl/voltijd-opleidingen/hbo-ict" target="_blank">HBO ICT - Software Development</a> at the Hogeschool Utrecht (2022 - 2026).
+            I've specialized myself in Front end & design, where I love to work with React and Typescript. But besides my specialization, I've also worked a ton with Back end related projects like Java Spring applications and PostgreSQL databases.
             <br><br>
-            My hobbies are learning Norwegian, working out in the gym, hanging out / gaming with friends, playing chess & going on a hike in the forest with my dog (Rowdy, Golden Retriever).
+            My hobbies are learning Norwegian, working out in the gym, hanging out / gaming with friends, playing chess & going on a hike in the forest with my dog (Rowdy, Golden Retriever). 
         `;
     }, 100);
 });
 
-function calculateAge() {
+const calculateAge = () => {
     let dob = new Date("01/20/2004");
     let month_diff = Date.now() - dob.getTime();
     let age_dt = new Date(month_diff);
