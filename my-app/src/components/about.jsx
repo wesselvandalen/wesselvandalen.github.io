6d3e7b45c321fc7387ecb43b1ea972f281151825
wessel <wesselvandalen@gmail.com>
2024-11-25 17:27:49 +0100

    remove cv

--- my-app/src/components/about.jsx
+++ my-app/src/components/about.jsx
@@ -13,15 +13,6 @@ export default function About() {
         return Math.abs(year - 1970);
     };
 
-    const handleResumeButton = () => {
-        const currentLanguage = localStorage.getItem('lang');
-        const url = 'https://github.com/wesselvandalen/wesselvandalen.github.io/blob/main/cv.pdf'; 
-
-        // Different CV's per language
-
-        window.open(url);
-    }
-
     return (
         <>
             <Header />
@@ -43,13 +34,6 @@ export default function About() {
                         <a href="https://linkedin.com/in/wesselvandalen/" target='_blank' className='lenke'>LinkedIn</a>
                         <a href="mailto:wesselvandalen@gmail.com" className='lenke'>wesselvandalen@gmail.com</a>
                     </div>
-
-                    <button className='cv-button' onClick={handleResumeButton}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                        </svg>
-                        {translation('about.cv')}
-                    </button>
                 </div>
             </div>
         </>
