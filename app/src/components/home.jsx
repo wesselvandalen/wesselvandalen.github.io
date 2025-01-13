f41497d25de5985699f9261f693ffd3dd4368fb8
wessel <wesselvandalen@gmail.com>
2025-01-13 23:54:41 +0100

    sfd

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import meg from '../assets/kopi.png';
 import linkedinIcon from '../assets/icons/linkedin.png';
 import githubIcon from '../assets/icons/githubgray.png';
+import stackoverflowIcon from '../assets/icons/stackoverflowgray.png';
 
 export default function Home() {
     const [t] = useTranslation("global");
@@ -40,22 +41,22 @@ export default function Home() {
                     <h2 className='role'>{t('home.role')}</h2>
                 </div>
                 
-                {/* <div className="social-container">
+                <div className="social-container">
                     <a href="mailto:wesselvandalen@gmail.com">
                         <svg width="90%" height="90%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.4995 13.5001L20.9995 3.00005M10.6271 13.8281L13.2552 20.5861C13.4867 21.1815 13.6025 21.4791 13.7693 21.566C13.9139 21.6414 14.0862 21.6415 14.2308 21.5663C14.3977 21.4796 14.5139 21.1821 14.7461 20.587L21.3364 3.69925C21.5461 3.16207 21.6509 2.89348 21.5935 2.72185C21.5437 2.5728 21.4268 2.45583 21.2777 2.40604C21.1061 2.34871 20.8375 2.45352 20.3003 2.66315L3.41258 9.25349C2.8175 9.48572 2.51997 9.60183 2.43326 9.76873C2.35809 9.91342 2.35819 10.0857 2.43353 10.2303C2.52043 10.3971 2.81811 10.5128 3.41345 10.7444L10.1715 13.3725C10.2923 13.4195 10.3527 13.443 10.4036 13.4793C10.4487 13.5114 10.4881 13.5509 10.5203 13.596C10.5566 13.6468 10.5801 13.7073 10.6271 13.8281Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
-                        <span className="home-tooltip">E-mail</span>
                     </a>
                     <a href="https://github.com/wesselvandalen" target='_blank' rel='noreferrer'>
                         <img src={githubIcon} alt="GitHub icon" />
-                        <span className="home-tooltip">GitHub</span>
                     </a>
                     <a href="https://linkedin.com/in/wesselvandalen" target='_blank' rel='noreferrer'>
                         <img src={linkedinIcon} alt="LinkedIn icon" />
-                        <span className="home-tooltip">LinkedIn</span>
                     </a>
-                </div> */}
+                    <a href="https://stackoverflow.com/users/24017710/wesseldalen" target='_blank' rel='noreferrer'>
+                        <img src={stackoverflowIcon} alt="Stack Overflow icon" />
+                    </a>
+                </div>
 
                 <p className='description'>Hallaisen! {t('home.description1').replace('{age}', calculateAge()).replace('{exp}', calculateExperience())}</p>
                 <p className='description'>{t('home.description2')}</p>
--- app/src/components/side-panel.jsx
+++ app/src/components/side-panel.jsx
@@ -1,7 +1,6 @@
 import './side-panel.css';
 import { useTranslation } from 'react-i18next';
 import './language-menu.css';
-import { useState, useEffect } from 'react';
 import profile from '../assets/kopi.png';
 import linkedinIcon from '../assets/icons/linkedin.png';
 import githubIcon from '../assets/icons/githubgray.png';
@@ -9,19 +8,7 @@ import stackoverflowIcon from '../assets/icons/stackoverflowgray.png';
 
 export default function SidePanel() {
     const [t, i18n] = useTranslation("global");
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const [width, setWidth] = useState(window.innerWidth);
     const strokeWidth = 1.5;
-
-    useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth);
-    
-        window.addEventListener('resize', handleResize);
-    
-        return () => window.removeEventListener('resize', handleResize);
-      }, []); 
-
-    const toggleMenu = () => setMenuOpen(!isMenuOpen);
 
     const handleLanguageSwitch = (lang) => {
         i18n.changeLanguage(lang);
@@ -40,15 +27,7 @@ export default function SidePanel() {
     return (
         <div className="panel-container">
 
-            <button className="hamburger-menu" onClick={toggleMenu}>
-                <span className="hamburger-icon">
-                    <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3 12H21M3 6H21M9 18H21" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                </span>
-            </button>
-
-            <nav className={`panel-nav ${isMenuOpen ? 'open' : ''}`}>
+            <nav className="panel-nav">
 
                 <div className="panel-profile">
                     <img src={profile} alt="Me" />
@@ -57,7 +36,7 @@ export default function SidePanel() {
 
                 <div className="line" />
 
-                <div className="panel-content panel-chapters">
+                <div className="panel-content">
 
                     <a href="#home">
                         <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -128,7 +107,7 @@ export default function SidePanel() {
 
                 <div className="line" />
 
-                <div className="panel-content panel-socials" style={{ display: `${(isMenuOpen || width > 1080) ? 'flex' : 'none'}` }}>
+                <div className="panel-content">
                     <a href="mailto:wesselvandalen@gmail.com">
                         <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.4995 13.5001L20.9995 3.00005M10.6271 13.8281L13.2552 20.5861C13.4867 21.1815 13.6025 21.4791 13.7693 21.566C13.9139 21.6414 14.0862 21.6415 14.2308 21.5663C14.3977 21.4796 14.5139 21.1821 14.7461 20.587L21.3364 3.69925C21.5461 3.16207 21.6509 2.89348 21.5935 2.72185C21.5437 2.5728 21.4268 2.45583 21.2777 2.40604C21.1061 2.34871 20.8375 2.45352 20.3003 2.66315L3.41258 9.25349C2.8175 9.48572 2.51997 9.60183 2.43326 9.76873C2.35809 9.91342 2.35819 10.0857 2.43353 10.2303C2.52043 10.3971 2.81811 10.5128 3.41345 10.7444L10.1715 13.3725C10.2923 13.4195 10.3527 13.443 10.4036 13.4793C10.4487 13.5114 10.4881 13.5509 10.5203 13.596C10.5566 13.6468 10.5801 13.7073 10.6271 13.8281Z" stroke="currentColor" strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round" />
