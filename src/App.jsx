3d2da98c414e809aac4f93c3d90396b3fc1283a6
wessel <wesselvandalen@gmail.com>
2025-02-13 23:24:16 +0100

    Startet prosjekt på nytt

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+
+  return (
+    <>
+      <p>Hallo</p>
+    </>
+  )
+}
---
+++ src/pages/main.jsx
@@ -0,0 +1,12 @@
+import { useTranslation } from 'react-i18next';
+import './main.css';
+
+export default function Main() {
+    const [t] = useTranslation("global");
+
+    return (
+        <div className="main-container">
+
+        </div>
+    );
+}
